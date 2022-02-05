612f235504f48ede7245aaf3049eda2d3f7e4662
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-02-06 04:21:07 +0530

    feat: added rating component

---
+++ components/rating/rating.js
@@ -0,0 +1,13 @@
+const ratingInputs = document.getElementsByClassName("rating-input");
+const resultContainer = document.getElementById("example-display");
+
+for(let i = 0; i < ratingInputs.length; i++){
+    ratingInputs[i].addEventListener("click", function(){
+        resultContainer.classList.remove("hidden");
+        resultContainer.innerHTML = `<h4>Thanks for rating the product!</h4> <p>Your rating: ${this.value}/5.</p>`
+    })
+}
+
+
+
+
